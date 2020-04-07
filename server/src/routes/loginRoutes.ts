cc7fa29c8048e1b9090ea756b2e02bbffe48ab72
Malgorzata <mkrzek@googlemail.com>
2020-04-07 13:46:42 +0100

    feat: add logout and root route

--- server/src/routes/loginRoutes.ts
+++ server/src/routes/loginRoutes.ts
@@ -1,10 +1,28 @@
-import { Router, Request, Response } from "express";
+import { Router, Request, Response, NextFunction } from "express";
 interface RequestWithBody extends Request {
   body: { [key: string]: string | undefined };
 }
 const router = Router();
 
-router.get("/login", (req: Request, res: Response) => {
+router.get("/", (req: Request, res: Response) => {
+  if (req.session && req.session.loggedIn) {
+    res.send(`
+    <div>
+      <div>You are logged in</div>
+      <a href="/logout">Logout</a>
+    </div>
+    `);
+  } else {
+    res.send(`
+    <div>
+      <div>You are not logged in</div>
+      <a href="/login">Logout</a>
+    </div>
+    `);
+  }
+});
+
+router.get("/login", (req: RequestWithBody, res: Response) => {
   res.send(`
        <form method="POST">
         <div>
@@ -22,11 +40,16 @@ router.get("/login", (req: Request, res: Response) => {
 router.post("/login", (req: RequestWithBody, res: Response) => {
   const { email, password } = req.body;
 
-  if (email && password) {
-    res.send(email + password);
+  if (email && password && email === "hi@hi.com" && password === "password") {
+    req.session = { loggedIn: true };
+    res.redirect("/");
   } else {
-    res.send("You must provide an email");
+    res.send("Invalid email or password");
   }
+});
+
+router.get("/logout", (res: Request, req: Response) => {
+  req.session = undefined;
 });
 
 export { router };
