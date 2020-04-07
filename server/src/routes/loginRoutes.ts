f09a5174f40a17c9f90575df4e90b83213443276
Malgorzata <mkrzek@googlemail.com>
2020-04-07 12:30:47 +0100

    feat: add interface for req.body and type guard

--- server/src/routes/loginRoutes.ts
+++ server/src/routes/loginRoutes.ts
@@ -1,7 +1,10 @@
-import {Router, Request, Response} from 'express'
+import { Router, Request, Response } from "express";
+interface RequestWithBody extends Request {
+  body: { [key: string]: string | undefined };
+}
+const router = Router();
 
-const router = Router()
-router.get('/login', (req : Request, res : Response) => {
+router.get("/login", (req: Request, res: Response) => {
   res.send(`
        <form method="POST">
         <div>
@@ -15,10 +18,15 @@ router.get('/login', (req : Request, res : Response) => {
         <button>submit</button>
        </form>
     `);
-})
-router.post('/login', (req : Request, res : Response) => {
-  const {email, password} = req.body
-  res.send(email + password)
-})
+});
+router.post("/login", (req: RequestWithBody, res: Response) => {
+  const { email, password } = req.body;
 
-export {router}
+  if (email && password) {
+    res.send(email + password);
+  } else {
+    res.send("You must provide an email");
+  }
+});
+
+export { router };
