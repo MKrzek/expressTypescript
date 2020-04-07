b6f33111c6dca5af035f8b4423ee9890dfa468fc
Malgorzata <mkrzek@googlemail.com>
2020-04-07 13:59:33 +0100

    feat: add cookie session

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,13 @@
-import express, {Request, Response} from 'express'
-import bodyParser from 'body-parser'
-import {router} from './routes/loginRoutes'
+import express, { Request, Response } from "express";
+import bodyParser from "body-parser";
+import { router } from "./routes/loginRoutes";
+import cookieSession from "cookie-session";
 
-const app = express()
-app.use(bodyParser.urlencoded({extended: true}));
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieSession({ keys: ["djkjjkdjdk"] }));
 app.use(router);
 
 app.listen(3000, () => {
-  console.log('listening on port 3000')
-})
+  console.log("listening on port 3000");
+});
