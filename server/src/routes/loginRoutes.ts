6a7e9d0f0686f0efff1feea8f7d150bd0a0d6b44
Malgorzata <mkrzek@googlemail.com>
2020-04-07 13:59:12 +0100

    feat: add middleware

--- server/src/routes/loginRoutes.ts
+++ server/src/routes/loginRoutes.ts
@@ -2,6 +2,16 @@ import { Router, Request, Response, NextFunction } from "express";
 interface RequestWithBody extends Request {
   body: { [key: string]: string | undefined };
 }
+
+function requireAuth(req: Request, res: Response, next: NextFunction): void {
+  if (req.session && req.session.loggedIn) {
+    next();
+    return;
+  }
+  res.status(403);
+  res.send("Not permitted");
+}
+
 const router = Router();
 
 router.get("/", (req: Request, res: Response) => {
@@ -16,7 +26,7 @@ router.get("/", (req: Request, res: Response) => {
     res.send(`
     <div>
       <div>You are not logged in</div>
-      <a href="/login">Logout</a>
+      <a href="/login">Log in</a>
     </div>
     `);
   }
@@ -48,8 +58,13 @@ router.post("/login", (req: RequestWithBody, res: Response) => {
   }
 });
 
-router.get("/logout", (res: Request, req: Response) => {
+router.get("/logout", (req: Request, res: Response) => {
   req.session = undefined;
+  res.redirect("/");
+});
+
+router.get("/protected", requireAuth, (req: Request, res: Response) => {
+  res.send("Welcome to protected route, logged in user");
 });
 
 export { router };
